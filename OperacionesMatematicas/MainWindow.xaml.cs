b7613c62a7aecbe6e2b00429fde80c9c10bd37ef
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2019-09-25 12:57:42 +0200

    Actualizacion calculadora

--- OperacionesMatematicas/MainWindow.xaml.cs
+++ OperacionesMatematicas/MainWindow.xaml.cs
@@ -20,9 +20,80 @@ namespace OperacionesMatematicas
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Operando1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            if (RadioSuma.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 + operando2);
+            if (RadioResta.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 - operando2);
+            if (RadioMultiplicacion.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 * operando2);
+            if (RadioDivision.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 / operando2);
+
+        }
+
+        private void Operando2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            if (RadioSuma.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 + operando2);
+            if (RadioResta.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 - operando2);
+            if (RadioMultiplicacion.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 * operando2);
+            if (RadioDivision.IsChecked == true)
+                ResultadoOperando.Text = Convert.ToString(operando1 / operando2);
+        }
+
+        private void RadioSuma_Checked(object sender, RoutedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            ResultadoOperando.Text = Convert.ToString(operando1 + operando2);
+        }
+
+        private void RadioResta_Checked(object sender, RoutedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            ResultadoOperando.Text = Convert.ToString(operando1 - operando2);
+        }
+
+        private void RadioMultiplicacion_Checked(object sender, RoutedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            ResultadoOperando.Text = Convert.ToString(operando1 * operando2);
+        }
+
+        private void RadioDivision_Checked(object sender, RoutedEventArgs e)
+        {
+            int operando1 = Convert.ToInt32(Operando1.Text);
+            int operando2 = Convert.ToInt32(Operando2.Text);
+            ResultadoOperando.Text = Convert.ToString(operando1 / operando2);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RadioSuma.IsChecked = false;
+            RadioResta.IsChecked = false;
+            RadioMultiplicacion.IsChecked = false;
+            RadioDivision.IsChecked = false;
+            Operando1.Text = "0";
+            Operando2.Text = "0";
+            ResultadoOperando.Text = "0";
+
         }
     }
 }
